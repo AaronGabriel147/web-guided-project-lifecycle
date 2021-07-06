19518c93b4bfab21f14a40d87d2ec4f2cf328cc5
Warren Longmire <warren.longmire@lambdaschool.com>
2021-07-06 12:42:11 -0400

    lecture work-in-progress

--- review/src/AppClass.js
+++ review/src/AppClass.js
@@ -14,6 +14,13 @@ class AppClass extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    console.log('old props: ', prevProps);
+    console.log('new props: ', this.props);
+
+    console.log('old state: ', prevState);
+    console.log('new state: ', this.state);
+
+
     console.log("AppClass: Component Updates");
   }
 
