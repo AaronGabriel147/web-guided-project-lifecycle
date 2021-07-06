e457ebe36c9d2bab1cdc9f8683a0a4b410953c9a
Warren Longmire <warren.longmire@lambdaschool.com>
2021-07-06 13:23:05 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-  render() {
-    const dogImages = [
-      "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-      "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-      "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-      "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
-    ];
+  constructor() {
+    super();
+    this.state = {
+      dogImages: [
+        "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
+        "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
+        "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
+        "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
+      ]
+    }
+  }
 
+  render() {
     return (<div>
       <h1>Dog Search Form v1.0</h1>
       <form>
