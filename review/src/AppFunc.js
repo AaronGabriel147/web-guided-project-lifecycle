ec3afcaec22fe60f6aca34c3bce64a43cf11e14e
Warren Longmire <warren.longmire@lambdaschool.com>
2021-07-06 12:26:17 -0400

    lecture work-in-progress

--- review/src/AppFunc.js
+++ review/src/AppFunc.js
@@ -6,10 +6,12 @@ const AppFunc = () => {
 
   useEffect(() => {
     console.log("AppFunc: Component Mounts");
+    setName();
   }, []);
 
   useEffect(() => {
     console.log("AppFunc: Component Updates");
+    setName();
   })
 
   const handleClick = () => {
