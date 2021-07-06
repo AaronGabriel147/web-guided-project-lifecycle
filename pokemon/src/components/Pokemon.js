8b8abba56e5728c68448bd5fe1754eed6b06e5f1
Warren Longmire <warren.longmire@lambdaschool.com>
2021-07-06 12:50:30 -0400

    lecture work-in-progress

--- pokemon/src/components/Pokemon.js
+++ pokemon/src/components/Pokemon.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Poki from './Poki';
 
 class Pokemon extends React.Component {
+  componentDidMount() {
+    console.log("Pokemon: Component Mounts");
+  }
   render() {
+    console.log("Pokemon: Renders DOM");
     return (
       <>
         {this.props.pokemon.map(pokemon => (
-          <Poki key={pokemon.id} pokemon={pokemon}/>
+          <Poki key={pokemon.id} pokemon={pokemon} />
         ))}
       </>
     );
